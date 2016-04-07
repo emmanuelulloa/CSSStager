65ae1fd9884eac19595f5c747eb2788a8595497e
Emmanuel Ulloa Arguello <emmanuelulloa@users.noreply.github.com>
2016-04-06 23:59:30 -0600

    Added new functions

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,11 @@
 utils = {
 	clamp : function(value, min, max){
 		return Math.min(Math.max(value,min),max);
+	},
+	replaceAll : function(target, search, replacement){
+		return target.replace(new RegExp(search,'g'), replacement);
+	},
+	hasString : function(target, search){
+		return target.indexOf(search) != -1;
 	}
 }
